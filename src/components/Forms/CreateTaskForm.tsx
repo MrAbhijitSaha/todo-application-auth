c36951e09dea3c4a33a097534119ba96cc8864f0
Abhijit Saha <work.abhijitsaha@gmail.com>
2026-01-10 18:54:25 +0530

    Implement CreateTaskForm with validation
    Replaced placeholder CreateTaskForm with a functional form using react-hook-form and zod for validation. Added input field for task name, integrated error handling, and connected to shadcnui components.

--- src/components/Forms/CreateTaskForm.tsx
+++ src/components/Forms/CreateTaskForm.tsx
@@ -1,9 +1,50 @@
+import { taskDataSchema, TaskDataType } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { Field, FieldError, FieldLabel } from "../shadcnui/field";
+import { Input } from "../shadcnui/input";
+
 const CreateTaskForm = () => {
-    return (
-        <div>
-            Enter
-        </div>
-    );
-}
+	const {
+		handleSubmit,
+		control,
+		reset,
+		formState: { isSubmitting },
+		setValue,
+		clearErrors,
+	} = useForm({
+		resolver: zodResolver(taskDataSchema),
+		defaultValues: {
+			task: "",
+		},
+		mode: "all",
+	});
+
+	const formHandelar = (value: TaskDataType) => {
+		console.log(value);
+	};
+
+	return (
+		<form onSubmit={handleSubmit(formHandelar)}>
+			<Controller
+				name="task"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Enter Your Task Name</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							aria-invalid={fieldState.invalid}
+							placeholder="Enter Your Task Name"
+							autoComplete="off"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+		</form>
+	);
+};
 
 export default CreateTaskForm;
